201bba36ef7fa78bc7f2839287b52163b17fbcf6
Kundan <ntamsoy662gmail.com>
2023-05-12 01:05:22 +0530

    added js file

---
+++ script.js
@@ -0,0 +1,20 @@
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    let a;
+
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
+    }
+    let Color1 = populate('#');
+    let Color2 = populate('#');
+    var angle = 'to left top';
+
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")fixed";
+    document.body.style.background = gradient;
+}
+random_bg_color();
