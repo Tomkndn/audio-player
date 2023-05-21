8861e227c19be0614f722b963c589338c659d89e
Kundan <ntamsoy662gmail.com>
2023-05-21 11:28:49 +0530

    added background changer

--- script.js
+++ script.js
@@ -158,22 +158,22 @@ function setUpdate(){
         total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
 }
-// function random_bg_color(){
-//     let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-//     let a;
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    let a;
 
-//     function populate(a){
-//         for(let i=0; i<6; i++){
-//             let x = Math.round(Math.random() * 15);
-//             let y = hex[x];
-//             a += y;
-//         }
-//         return a;
-//     }
-//     let Color1 = populate('#');
-//     let Color2 = populate('#');
-//     var angle = 'to bottom right';
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 15);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
+    }
+    let Color1 = populate('#');
+    let Color2 = populate('#');
+    var angle = 'to bottom right';
 
-//     let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")fixed";
-//     document.body.style.background = gradient;
-// }
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")fixed";
+    document.body.style.background = gradient;
+}
